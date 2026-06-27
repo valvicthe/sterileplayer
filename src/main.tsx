031106a626fb1fd8e61e9ec24167eb8ec07ddb97
valvicthe <louie12brett@icloud.com>
2026-06-27 15:06:48 +0100

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import Sterile from './LarpMedia' // 1. Capitalized the import alias
+import LarpMedia from './LarpMedia' // 1. Capitalized the import alias
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <Sterile /> {/* 2. Capitalized the component tag */}
+    <LarpMedia /> {/* 2. Capitalized the component tag */}
   </React.StrictMode>,
 )
